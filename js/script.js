d588ba1307e2f7c1ec7452626029869efdc0d926
cerry24 <samuelecerretti@gmail.com>
2022-11-22 18:27:18 +0100

    implement output

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 const inputRouteElement = document.getElementById ('routeLength');
 const inputAgeElement = document.getElementById ('passengerAge');
 
-const button = document.getElementById ('btn-submit');
+const button = document.getElementById ('ms_btn-submit');
 
 
 button.addEventListener ('click', function(){
     let grossPrice = parseInt(inputRouteElement.value) * 0.2762;
 
-    console.log (grossPrice);
+    // console.log (grossPrice);
 
     let finalPrice = grossPrice;
 
     if ( parseInt(inputAgeElement.value) < 18 ) {
         finalPrice = (grossPrice - (grossPrice * 0.175));
-        console.log (finalPrice);
+        // console.log (finalPrice);
+        
     } else if ( parseInt(inputAgeElement.value) > 65 ) {
         finalPrice = (grossPrice - (grossPrice * 0.333));
-        console.log (finalPrice);
+        // console.log (finalPrice);
     } else {
-        console.log (finalPrice);
+        // console.log (finalPrice);
     }
+
+    alert (`Il costo totale del suo biglietto é ${finalPrice.toFixed(2)}€, grazie per averci scelto.`)
 })
