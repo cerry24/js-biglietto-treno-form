18794a9e00f5d60c7a8a991e3bd361845f1171eb
cerry24 <samuelecerretti@gmail.com>
2022-11-22 16:07:59 +0100

    implement script.js funcional ticket calculator

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const inputRouteElement = document.getElementById ('routeLength');
+const inputAgeElement = document.getElementById ('passengerAge');
+
+const button = document.getElementById ('btn-submit');
+
+
+button.addEventListener ('click', function(){
+    let grossPrice = parseInt(inputRouteElement.value) * 0.2762;
+
+    console.log (grossPrice);
+
+    let finalPrice = grossPrice;
+
+    if ( parseInt(inputAgeElement.value) < 18 ) {
+        finalPrice = (grossPrice - (grossPrice * 0.175));
+        console.log (finalPrice);
+    } else if ( parseInt(inputAgeElement.value) > 65 ) {
+        finalPrice = (grossPrice - (grossPrice * 0.333));
+        console.log (finalPrice);
+    } else {
+        console.log (finalPrice);
+    }
+})
